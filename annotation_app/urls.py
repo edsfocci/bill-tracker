275b2b6232e3c0f04cabb621fd3fc9a6d91505ed
deeptiboddapati <deeptiboddapati@gmail.com>
2015-06-10 19:53:41 -0500

    figuring out todos

--- annotation_app/urls.py
+++ annotation_app/urls.py
@@ -17,18 +17,18 @@ from django.conf.urls import include, url
 from annotation_app import views
 
 urlpatterns = [
-    url(r'^index/$', views.index),
-    url(r'^$', views.bill_list, name='bill_list'),
-    url(r'^bills/$', views.bill_list, name='bills'),
-    url(r'^addbill/$', views.add_bill, name='add_bill'),
-    url(r'^bills/(?P<bill_id>\d+)/$', views.bill, name='bill'),
-    url(r'^bills/(?P<bill_id>\d+)/edit/$', views.edit_bill, name='edit_bill'),
+    #url(r'^index/$', views.index),
+    url(r'^$', views.bill_list, name='bill_list'),#shows list of bills current homepage
+    url(r'^bills/$', views.bill_list, name='bills'),#same as bill list/root
+    url(r'^addbill/$', views.add_bill, name='add_bill'),#allows user to add bill. 
+    url(r'^bills/(?P<bill_id>\d+)/$', views.bill, name='bill'), #shows a specific bill
+    url(r'^bills/(?P<bill_id>\d+)/edit/$', views.edit_bill, name='edit_bill'),#allows us to edit it.
     #url(r'^addannotation/$', views.add_annotation, name='add_annotation'),
     #url(r'^annotations/(?P<annotation_id>\d+)/$', views.annotation,
     #  name='annotation'),
     #url(r'^addcomment/$', views.add_comment, name='add_comment'),
     #url(r'^comments/(?P<comment_id>\d+)/$', views.comment, name='comment'),
-    #url(r'^example-client/$', views.example_client, name='example_client'),
-    #url(r'^getbill/$', views.get_bill, name='get_bill'), this view is not needed
+    url(r'^example-client/$', views.example_client, name='example_client'), #shows how to use the json
+    url(r'^getbill/$', views.get_bill, name='get_bill'), #gives json version of the bill 
     url(r'^megalith/$', views.megalith, name='megalith'), #TODO fold this into annotations
 ]
--- annotation_app/views.py
+++ annotation_app/views.py
@@ -142,3 +142,51 @@ def example_client(request):
 
 def megalith(request):
   return render(request, 'megalith/megalith.html')
+  
+def get_bill(request):
+  print(request.GET)
+  bill = Bill()
+  bill.number = request.GET['bill_number']
+  # #bill_txt = get_bill_text(str(bill.number))
+  # #print("Checkpoint2")
+  # #if (bill_txt == None):
+  # #  return Http404
+  # #else:
+  # #  bill.text = bill_txt
+  # #subjects = get_history("SB", str(bill.number))
+  # #bill.subjects = Bill.serialize(subjects)
+  # print("Checkpoint2")
+  # ####
+  billsb10 = Bill_Import()
+  billsb10.set_bill_num('10')
+  billsb10.pull_billtext()
+  bill_list = billsb10.billtext
+  bill.text = Bill.serialize(bill_list)
+  # print("Checkpoint3")
+  billsb10.pull_history()
+  # print("Checkpoint4")
+  billsb10.set_authors()
+  bill.authors = Bill.serialize(billsb10.authors)
+  # print("Checkpoint5")
+  billsb10.set_coauthors()
+  bill.coauthors = Bill.serialize(billsb10.coauthors)
+  # print("Checkpoint5")
+  billsb10.set_subjects()
+  bill.subjects = Bill.serialize(billsb10.subjects)
+  # print("Checkpoint6")
+  billsb10.set_cosponsors()
+  bill.cosponsors = Bill.serialize(billsb10.cosponsors)
+  # print("Checkpoint7")
+  billsb10.set_sponsors()
+  bill.sponsors = Bill.serialize(billsb10.sponsors)
+  # print('authors', Bill.deserialize(bill.authors))
+  # print('billtext', Bill.deserialize(bill.text))
+  # print('subjects', Bill.deserialize(bill.subjects))
+  # print('coauthors', Bill.deserialize(bill.coauthors))
+  # print('sponsors', Bill.deserialize(bill.sponsors))
+  # print('cosponsors', Bill.deserialize(bill.cosponsors))
+
+  # ####
+
+  bill.save()
+  return HttpResponse(serialize('json', [bill]))
