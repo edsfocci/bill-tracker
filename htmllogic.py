910f62401eb240a89e37e36c24717f45bd974573
deeptiboddapati <deeptiboddapati@gmail.com>
2015-06-11 23:49:27 -0500

    nonfunctional html consolidator

--- htmllogic.py
+++ htmllogic.py
@@ -6,49 +6,51 @@ Created on Wed Jun 10 22:04:34 2015
 /deeptiboddapati
 """
 import bs4, re
+
 def remove_empty_tag():
-    taglist = ['td','tr','u']
-    file = open('file.html', 'r')
-    writef = open('destfile.html', 'w')
-    
-    domfile = bs4.BeautifulSoup(file.read())
+    taglist = ['td','tr','u']   
     
     for x in range(len(taglist)):
         i = 0
-        while i < (len(domfile(taglist[x]))):
-            if domfile(taglist[x])[i].getText().isspace():
-                domfile(taglist[x])[i].extract()
+        while i < (len(lines(taglist[x]))):
+            if lines(taglist[x])[i].getText().isspace():
+                lines(taglist[x])[i].extract()
             else:
                 i +=1
-    #self.billtext[-1]=domfile.prettify()
-    writef.write(str(domfile.prettify()))
-    writef.close()
-    file.close()
-
+'''
 def remove_title():
     file = open('destfile.html', 'r')    
-    domfile = bs4.BeautifulSoup(file.read())
-    title = re.sub(r'(\s\s+)',r' ', domfile('title')[0].extract().get_text())
+    lines = bs4.BeautifulSoup(file.read())
+    title = re.sub(r'(\s\s+)',r' ', lines('title')[0].extract().get_text())
     #gets the second table in text which contains the bill. 
-    domfile = domfile('table')[1].extract()
+    lines = lines('table')[1].extract()
     #gets the first sentence of the bill.
-    for x in domfile('td'):
+    for x in lines('td'):
         chkstring = x.get_text() #TODO ask mark if he wants the text extracted.
         b = re.search('[.]',chkstring)
         title += re.sub(r'(\s\s+)',r' ', chkstring) 
         if b is not None:
             break
     writef = open('destfile.html', 'w')
-    writef.write(domfile.prettify())
+    writef.write(lines.prettify())
     writef.close()
     return title
-    
+'''
 def consolidate_tag():
-    #some consolidation.
-
+    file = open('destfile.html', 'r')    
+    lines = bs4.BeautifulSoup(file.read())
+    file.close()
+    for x in lines('tr'):
+        for i in x.descendants:
+            for y in i.find_next_siblings():
+                if y is None:
+                    y.extract()
+                elif y.name == i.name:
+                    print(y.string)
+    print(i)
 def main():
     remove_empty_tag()
-    title = remove_title()
-    print([title])
-    
+    #title = remove_title()
+    #print([title])
+    consolidate_tag()
 main()
