1705ba3cb8b231c9cd90588e4cc9ee4aeaaceb97
deeptiboddapati <deeptiboddapati@gmail.com>
2015-06-15 21:52:53 -0500

    finished setting up markers and classes.

--- htmltest1.py
+++ htmltest1.py
@@ -30,5 +30,15 @@ sec3 = re.compile("\A(SUBCHAPTER)")
 sec4 = re.compile("\A(\(.\))(?!\s\(.\))")
 "\A(\(.\))(?!\(.\))"
 
-a = re.search(sec4, "(x)")
-print(a)
+regcheck = [[sec, 'SECTION', "p" ], [sec1, "Sec", 'p' ],[sec3,"Subchapter", "p"],[sec4, "list", "li" ]]
+
+#find each expression and add the classes above to it.
+
+#print(re.search(sec, list(lines("tr")[-4].stripped_strings)[0]))
+for x in regcheck:#TODO google double for
+	for i in lines('tr'):
+	        #check what the first word is
+	        if re.search(x[0], list(i.stripped_strings)[0]):
+	            i.name = x[2]
+	            i['class'] = x[1]
+	            print(i)
