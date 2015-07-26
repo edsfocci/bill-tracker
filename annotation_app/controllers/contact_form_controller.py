7a95d477514deca858004aa61e752635bd14f4ef
deeptiboddapati <deeptiboddapati@gmail.com>
2015-07-26 00:53:22 -0500

    adding sending ability to form

--- annotation_app/controllers/contact_form_controller.py
+++
@@ -1,5 +0,0 @@
-from django.core import serializers
-from django.http import HttpResponseRedirect, HttpResponse
-from django.shortcuts import render
-from django.views.decorators.csrf import ensure_csrf_cookie
-import json
