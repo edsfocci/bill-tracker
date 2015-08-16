8e2e07d984417dcf04fdd0257e74a565df5fb495
Ed Solis <edsfocci@gmail.com>
2015-08-16 07:33:36 -0500

    Fix things the merging broke

--- static/global.js
+++ static/global.js
@@ -86,7 +86,7 @@ if ( $('.billarea').length ) {
 	var annotations = [];
 	// about half of highlight height
 	// (this gets top of annotation to top of highlight)
-	var offset = 7; 
+	var offset = 7;
 	var basePos = $('.billarea').offset().top;
 
 }
@@ -177,7 +177,7 @@ function placeNewAnnotation (newAnnotation) {
 
 				if ( (targetTop >= testTop) && (targetTop <= testBot) ||
 					(targetBot >= testTop) && (targetBot <= testBot) ) {
-					
+
 					isCollision = true;
 					targetTop = testBot + offset;
 					targetBot = targetTop + annotationHeight;
@@ -188,18 +188,18 @@ function placeNewAnnotation (newAnnotation) {
 		}
 
 		if (!isCollision) {
-			
+
 			annotationTop = targetTop;
 
 		} else {
-			
+
 			isCollision = false;
 			testAnnotationCollision();
 
 		}
 
 	}
-	
+
 	// console.log('annotationTop: ' + annotationTop);
 	$('#submission #annotation-' + highlightId).css('top', annotationTop + 'px');
 
@@ -223,7 +223,7 @@ function loadAnnotation(index,highlightId,highlightTop,annotationHeight) {
 
 	if (index==0) {
 
-		annotationTop = highlightTop + offset;	
+		annotationTop = highlightTop + offset;
 
 	} else {
 
@@ -252,7 +252,8 @@ function loadAnnotation(index,highlightId,highlightTop,annotationHeight) {
 					var testTop = testAnnotation.topY;
 					var testBot = testAnnotation.botY;
 
-					if ( (targetTop >= testTop) && (targetTop <= testBot) || (targetBot >= testTop) && (targetBot <= testBot) ) {
+					if ( (targetTop >= testTop) && (targetTop <= testBot) ||
+						(targetBot >= testTop) && (targetBot <= testBot) ) {
 						
 						isCollision = true;
 						targetTop = testBot + offset;
@@ -268,16 +269,17 @@ function loadAnnotation(index,highlightId,highlightTop,annotationHeight) {
 				annotationTop = targetTop + offset;
 
 			} else {
-				
+
 				isCollision = false;
 				testAnnotationCollision();
 
 			}
-			
+
 		}
 
 
-		// if ( (targetTop > prevTop) && (targetTop < prevBot) || (targetBot > prevTop) && (targetBot < prevBot) ) {
+		// if ( (targetTop > prevTop) && (targetTop < prevBot) ||
+		// 	(targetBot > prevTop) && (targetBot < prevBot) ) {
 		// 	annotationTop = prevBot + 15;
 		// } else {
 		// 	annotationTop = targetTop + offset;
@@ -296,3 +298,129 @@ function loadAnnotation(index,highlightId,highlightTop,annotationHeight) {
 	});
 
 }
+
+
+/* ------------ contact form validation -------------- */
+
+/* from http://codepen.io/jaycbrf/pen/iBszr */
+$(document).ready(function() {
+	$('#contact_form').bootstrapValidator({
+	// To use feedback icons, ensure that you use Bootstrap v3.1.0 or later
+	feedbackIcons: {
+	    valid: 'fa fa-check',
+	    invalid: 'fa fa-close',
+	    validating: 'fa fa-refresh'
+	},
+	fields: {
+	    first_name: {
+	        validators: {
+	                stringLength: {
+	                min: 2,
+	            },
+	                notEmpty: {
+	                message: 'Please supply your first name'
+	            }
+	        }
+	    },
+	     last_name: {
+	        validators: {
+	             stringLength: {
+	                min: 2,
+	            },
+	            notEmpty: {
+	                message: 'Please supply your last name'
+	            }
+	        }
+	    },
+	    email: {
+	        validators: {
+	            notEmpty: {
+	                message: 'Please supply your email address'
+	            },
+	            emailAddress: {
+	                message: 'Please supply a valid email address'
+	            }
+	        }
+	    },
+	    phone: {
+	        validators: {
+	            notEmpty: {
+	                message: 'Please supply your phone number'
+	            },
+	            phone: {
+	                country: 'US',
+	                message: 'Please supply a vaild phone number with area code'
+	            }
+	        }
+	    },
+	    address: {
+	        validators: {
+	             stringLength: {
+	                min: 8,
+	            },
+	            notEmpty: {
+	                message: 'Please supply your street address'
+	            }
+	        }
+	    },
+	    city: {
+	        validators: {
+	             stringLength: {
+	                min: 4,
+	            },
+	            notEmpty: {
+	                message: 'Please supply your city'
+	            }
+	        }
+	    },
+	    state: {
+	        validators: {
+	            notEmpty: {
+	                message: 'Please select your state'
+	            }
+	        }
+	    },
+	    zip: {
+	        validators: {
+	            notEmpty: {
+	                message: 'Please supply your zip code'
+	            },
+	            zipCode: {
+	                country: 'US',
+	                message: 'Please supply a vaild zip code'
+	            }
+	        }
+	    },
+	    comment: {
+	        validators: {
+	              stringLength: {
+	                min: 10,
+	                max: 200,
+	                message:'Please enter at least 10 characters and no more than 200'
+	            },
+	            notEmpty: {
+	                message: 'Please supply a description of your project'
+	            }
+	            }
+	        }
+	    }
+	})
+	.on('success.form.bv', function(e) {
+	    $('#success_message').slideDown({ opacity: "show" }, "slow") // Do something ...
+	        $('#contact_form').data('bootstrapValidator').resetForm();
+
+	    // Prevent form submission
+	    e.preventDefault();
+
+	    // Get the form instance
+	    var $form = $(e.target);
+
+	    // Get the BootstrapValidator instance
+	    var bv = $form.data('bootstrapValidator');
+
+	    // Use Ajax to submit form data
+	    $.post($form.attr('action'), $form.serialize(), function(result) {
+	        console.log(result);
+	    }, 'json');
+	});
+});
